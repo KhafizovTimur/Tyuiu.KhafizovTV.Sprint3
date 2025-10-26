c1a4b28d99cec23acfcff52ea0b8b9b9061b6c4c
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-10-26 19:36:11 +0300

    Add project files.

---
+++ Tyuiu.KhafizovTV.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KhafizovTV.Sprint3.Task0.V2.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 9;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 0.0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint3.Task0.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.KhafizovTV.Sprint3.Task0.V2.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint3.Task0.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Хафизов Т. В. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Оператор цикла for                                               *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #2                                                             *");
+            Console.WriteLine("* Выполнил: Хафизов Т. В. | ИСПб-25-1                                    *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет               *");
+            Console.WriteLine("* произведение ряда по формуле                                           *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 9;
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
