7822e081b642fbbd679fb2fbf98ae5d61cfad2eb
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-11-08 16:00:03 +0300

    Add project files.

---
+++ Tyuiu.KhafizovTV.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KhafizovTV.Sprint3.Task0.V2.Lib;
+
+
+namespace Tyuiu.KhafizovTV.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 9;
+
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+
+            double wait = 0.0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
