a04a2fbafbd03ba0b8b597455f2d1701811d8637
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-11-09 20:29:10 +0300

    Task4

---
+++ Tyuiu.KhafizovTV.Sprint3.Task4.V1.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KhafizovTV.Sprint3.Task4.V1.Lib
+{
+    public class DataService : ISprint3Task4V12
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 1;
+            for (int x = startValue; x <= stopValue; x++)
+                if (x == 0)
+                    break;
+                else
+                    res *= Math.Sin(x) / x;
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.KhafizovTV.Sprint3.Task4.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.KhafizovTV.Sprint3.Task4.V1.Lib;
+
+namespace Tyuiu.KhafizovTV.Sprint3.Task4.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 0.001;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
