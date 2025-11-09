fc69dd4de72c83af19c95b3fafcac98cef317cf5
GOMOPRO777 <GOMOPRO777@DESKTOP-PDL0G17>
2025-11-09 22:31:03 +0300

    Fix6

--- Tyuiu.KhafizovTV.Sprint3.Task6.V14.Lib/DataService.cs
+++ Tyuiu.KhafizovTV.Sprint3.Task6.V14.Lib/DataService.cs
@@ -6,12 +6,17 @@ namespace Tyuiu.KhafizovTV.Sprint3.Task6.V14.Lib
     {
         public int GetSumTheDivisors(int startValue, int stopValue)
         {
-            int x;
             int sum = 0;
-            for (x = startValue; x <= stopValue; x++)
-                for (int d = 1; d <= x; d++)
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 6; d <= x; d++)
+                {
                     if (x % d == 0 && d > 5)
+                    {
                         sum += d;
+                    }
+                }
+            }
             return sum;
         }
     }
